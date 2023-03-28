3d3f0cd867f4f618500060d854c42b8e284bc59a
sruthinsurabhi <sruthin23695@gmail.com>
2023-03-28 16:49:48 +0530

    back space button added and some interface changes made

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import tkinter as tk
 class Calculator:
     def __init__(self, master):
         self.master = master
-        master.geometry("500x500")
+        master.geometry("450x480")
         master.configure(bg='#ada692')
         master.title("Calculator")
 
         # Entry field for displaying the result
-        self.result_display = tk.Text(master, width=20, height=3, font=('Arial', 16))
+        self.result_display = tk.Text(master, width=25, height=2, font=('Arial', 16,'bold'))
         self.result_display.grid(row=1, column=0, columnspan=4, padx=5, pady=5)
+        self.result_display.config(bg="#faeded", fg='black', borderwidth=5, relief='ridge', pady=10)
 
         # Create buttons for the calculator
         self.create_button("7", 2, 0)
@@ -29,11 +30,15 @@ class Calculator:
         self.create_button("C", 5, 2)
         self.create_button("+", 5, 3)
         self.create_button("=", 6, 0, 1, 4)
+        self.create_button("⌫", 1, 4)  # Backspace button
+
+        # Bind the BackSpace key event to the delete_last() function
+        self.master.bind('<BackSpace>', lambda event: self.delete_last())
 
     def create_button(self, text, row, column, rowspan=1, columnspan=1):
-        button = tk.Button(self.master, text=text, width=5, height=2, font=('Arial', 16), command=lambda: self.button_click(text))
+        button = tk.Button(self.master, text=text, width=5, height=2, font=('Arial', 16,'bold'), command=lambda: self.button_click(text))
         button.grid(row=row, column=column, rowspan=rowspan, columnspan=columnspan, padx=5, pady=5)
-        button.config(bg="#a18948")
+        button.config(bg="#a18948",fg='#e3df6f')
 
     def button_click(self, text):
         if text == "=":
@@ -47,9 +52,17 @@ class Calculator:
         elif text == "C":
             # Clear the entry field
             self.result_display.delete("1.0", tk.END)
+        elif text == "⌫":  # Handle backspace button
+            self.delete_last()
         else:
             # Append the clicked button's text to the entry field
             self.result_display.insert(tk.END, text)
+
+    def delete_last(self):
+        # Delete the last character from the entry field
+        current = self.result_display.get("1.0", tk.END)
+        if len(current) > 1:
+            self.result_display.delete("end-2c")
 
 window= tk.Tk()
 calculator = Calculator(window)
